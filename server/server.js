e11fbd8e9f7de786c3e3fae8c1012db3190e6917
unknown <kathyrnyoung57@gmail.com>
2023-08-13 20:39:42 -0400

    dist folder created

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 // const routes = require('./routes');
 
 const app = express();
@@ -12,12 +14,13 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
+  introspection: true,
 });
 
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
   app.use(
@@ -43,9 +46,7 @@ const startApolloServer = async () => {
     db.once("open", () => {
       app.listen(PORT, () => {
         console.log(`🌍 Now listening on localhost:${PORT}`);
-        console.log(
-          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-        );
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
       });
     });
   };
