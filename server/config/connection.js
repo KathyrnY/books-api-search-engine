44e8246899ceb32a579b6444ad93dc0e56566606
unknown <kathyrnyoung57@gmail.com>
2023-08-20 15:31:07 -0400

    fix at resolving db connection issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
